f4ee25c972a2531e00413043269d1e07ad7a6a6d
tee-hee-hee <teeheehee@163.com>
2017-11-10 15:38:16 +0800

    first commit

---
+++ src/main/java/site/nafiy/App.java
@@ -0,0 +1,13 @@
+package site.nafiy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
